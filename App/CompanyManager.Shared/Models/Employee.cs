92dba49c4ded36cebb8ba6c2e55aa5fa2f2d0c34
Danila Samuseu <Danila_Samuseu@epam.com>
2020-01-21 15:34:54 +0300

    Base models has been added.

---
+++ App/CompanyManager.Shared/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.Shared.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RentCost { get; set; }
+
+        public int Income { get; set; }
+    }
+}
---
+++ App/CompanyManager.Shared/Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.Shared.Models
+{
+    class Purchase
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Cost { get; set; }
+
+        public int RentCost { get; set; }
+
+        public int Income { get; set; }
+    }
+}
