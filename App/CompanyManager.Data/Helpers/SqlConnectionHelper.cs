fc071212c7293ed027adc5813b8f668f6a007f9e
Danila Samuseu <Danila_Samuseu@epam.com>
2020-01-22 11:28:05 +0300

    Base repository implementation has been added.

---
+++ App/CompanyManager.Data/Helpers/SqlConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace CompanyManager.Data.Helpers
+{
+    public class SqlConnectionHelper
+    {
+        public string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "kiryl.database.windows.net";
+            builder.UserID = "gorylko";
+            builder.Password = "Rbhbkk78901234";
+            builder.InitialCatalog = "FinanceAnalyzer";
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ App/CompanyManager.Data/Repositoies/Realization/UserDtoRepository.cs
@@ -0,0 +1,59 @@
+﻿using CompanyManager.Data.Helpers;
+using CompanyManager.Data.Mappers;
+using CompanyManager.Data.Models;
+using CompanyManager.Data.Repositoies.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CompanyManager.Data.Repositoies.Realization
+{
+    public class UserDtoRepository : IUserDtoRepository
+    {
+        private readonly ProcedureExecutor _executor;
+        private readonly UserMapper _mapper;
+
+        public UserDtoRepository(ProcedureExecutor executor)
+        {
+            _executor = executor ?? throw new ArgumentNullException(nameof(executor));
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<UserDto> GetAll()
+        {
+            var dataSet = _executor.ExecuteDataSet("sp_select_all_users");
+
+            return _mapper.MapUserCollection(dataSet);
+        }
+
+        public UserDto GetById(int id)
+        {
+            var dataSet = _executor.ExecuteDataSet(
+                "sp_select_user_by_id",
+                new Dictionary<string, object>
+                {
+                    { "id", id },
+                });
+
+            return _mapper.MapUser(dataSet);
+        }
+
+        public void Save(UserDto obj)
+        {
+            _executor.ExecuteNonQuery("sp_insert_user", new Dictionary<string, object>
+            {
+                { "login", obj.Email },
+                { "password", obj.Password },
+                { "passwordSalt", obj.PasswordSalt },
+            });
+        }
+
+        public void Update(UserDto obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
