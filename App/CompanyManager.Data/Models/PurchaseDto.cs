55a7dce2fe89c21fe45ff371ea21af7c04079278
Kiryl Shamiakou <Kiryl_Shamiakou@epam.com>
2020-01-24 12:17:50 +0300

    added dto models for EF, removed useless repositories

---
+++ App/CompanyManager.Data/Models/PurchaseDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.Data.Models
+{
+    public class PurchaseDto
+    {
+        public int Id { get; set; }
+
+        public int EnterpriseId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal? Cost { get; set; }
+
+        public decimal? RentPrice { get; set; }
+
+        public decimal? Income { get; set; }
+
+        public virtual EnterpriseDto Enterprise { get; set; }
+    }
+}
---
+++ App/CompanyManager.Data/Models/RolesToPermissionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CompanyManager.Data.Models
+{
+    public class RolesToPermissionDto
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public int PermissionId { get; set; }
+
+        public virtual PermissionDto Permission { get; set; }
+
+        public virtual RoleDto Role { get; set; }
+    }
+}
