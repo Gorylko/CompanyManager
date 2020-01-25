7257abc1f98744ddaab5f4e83d0a6a8ef7f345f2
Yahor Zabotsin <yahor_zabotsin@epam.com>
2020-01-25 17:04:58 +0300

    CompanyManagerContetx ready

--- App/CompanyManager.Data/Context/CompanyManagerContext.cs
+++ App/CompanyManager.Data/Context/CompanyManagerContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace CompanyManager.Data.Context
 {
+    using CompanyManager.Data.Models;
     using Microsoft.EntityFrameworkCore;
 
     public class CompanyManagerContext : DbContext
@@ -8,5 +9,19 @@
                 : base(options)
         {
         }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Enterprise> Enterprises { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<WorkArea> WorkAreas { get; set; }
     }
 }
