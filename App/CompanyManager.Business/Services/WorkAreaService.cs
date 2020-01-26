07a1d6d558aa82384fb4f1cf89ccf68bf12045ad
Yahor Zabotsin <yahor_zabotsin@epam.com>
2020-01-26 20:49:00 +0300

    WorkArea service ready

--- App/CompanyManager.Business/Services/WorkAreaService.cs
+++ App/CompanyManager.Business/Services/WorkAreaService.cs
@@ -32,5 +32,73 @@
                 return uow.WorkAreaRepository.GetAll()?.Select(wa => wa.ToWorkArea());
             }
         }
+
+        public WorkArea GetByEnterpriseId(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("id must be more than 0", nameof(id));
+            }
+
+            using (var uow = UnitOfWork)
+            {
+                WorkAreaDto workAreaDto;
+                workAreaDto = uow.WorkAreaRepository.GetAll()
+                                                    .FirstOrDefault(wa => wa.EnterpriseId == id) ?? throw new ArgumentNullException(nameof(workAreaDto));
+                return workAreaDto?.ToWorkArea();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("id must be more than 0", nameof(id));
+            }
+
+            using (var uow = UnitOfWork)
+            {
+                WorkAreaDto workAreaDto;
+                workAreaDto = uow.WorkAreaRepository.GetById(id) ?? throw new ArgumentNullException(nameof(workAreaDto));
+                uow.WorkAreaRepository.Delete(workAreaDto);
+
+                //need to use try-catch block
+                uow.SaveChanges();
+            }
+        }
+
+        public void Update(WorkArea workArea)
+        {
+            if (workArea == null)
+            {
+                throw new ArgumentNullException(nameof(workArea));
+            }
+
+            using (var uow = UnitOfWork)
+            {
+                WorkAreaDto workAreaDto = uow.WorkAreaRepository.GetById(workArea.Id) ?? throw new ArgumentNullException(nameof(workAreaDto));
+                uow.WorkAreaRepository.Update(workAreaDto);
+
+                //need to use try-catch block
+                uow.SaveChanges();
+            }
+        }
+
+        public void Save(WorkArea workArea)
+        {
+            if (workArea == null)
+            {
+                throw new ArgumentNullException(nameof(workArea));
+            }
+
+            using (var uow = UnitOfWork)
+            {
+                var workAreaDto = workArea.ToWorkAreaDto();
+                uow.WorkAreaRepository.Add(workAreaDto);
+
+                //need to use try-catch block
+                uow.SaveChanges();
+            }
+        }
     }
 }
