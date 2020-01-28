94486fdeb487e4eefd55737cebc2a042357f80d3
Егор Заботин <zabotinegor@mail.ru>
2020-01-28 09:46:05 +0300

    full list of methods in generic repository

--- App/CompanyManager.Data/Repositories/GenericRepository/GenericRepository.cs
+++ App/CompanyManager.Data/Repositories/GenericRepository/GenericRepository.cs
@@ -1,6 +1,9 @@
 ﻿namespace CompanyManager.Data.Repositories.GenericRepository
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
     using CompanyManager.Data.Context;
     using Microsoft.EntityFrameworkCore;
@@ -22,6 +25,79 @@
             _dbSet.Add(entity);
         }
 
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity GetSingle(Expression<Func<TEntity, bool>> @where, params Expression<Func<TEntity, object>>[] navigationProperties)
+        {
+            IQueryable<TEntity> dbQuery = navigationProperties
+                .Aggregate<Expression<Func<TEntity, object>>, IQueryable<TEntity>>(_dbSet, (current, navigationProperty)
+                => current.Include(navigationProperty));
+
+            return dbQuery.FirstOrDefault();
+        }
+
+        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> @where, params Expression<Func<TEntity, object>>[] navigationProperties)
+        {
+            IQueryable<TEntity> dbQuery = navigationProperties
+                .Aggregate<Expression<Func<TEntity, object>>, IQueryable<TEntity>>(_dbSet, (current, navigationProperty)
+                => current.Include(navigationProperty));
+
+            return await dbQuery.FirstOrDefaultAsync(where);
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] navigationProperties)
+        {
+            IQueryable<TEntity> dbQuery = _dbSet;
+
+            if (filter != null)
+            {
+                dbQuery = dbQuery.Where(filter);
+            }
+
+            return navigationProperties.Aggregate(dbQuery, (current, navigationProperty) => current.Include(navigationProperty));
+        }
+
+        public IQueryable<TSubEntity> Get<TSubEntity>(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] navigationProperties) where TSubEntity : TEntity
+        {
+            return Get(filter, navigationProperties).OfType<TSubEntity>();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public void Load<TCollection>(TEntity entity, Expression<Func<TEntity, ICollection<TCollection>>> navigationProperty)
+            where TCollection : class
+        {
+
+        }
+
+        public void Load<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> navigationProperty)
+            where TProperty : class
+        {
+            _context.Entry(entity).Reference(navigationProperty).Load();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void PartialUpdate(TEntity entity, object updatedEntity)
+        {
+            _context.Entry(entity).CurrentValues.SetValues(updatedEntity);
+        }
+
         public void Delete(object id)
         {
             TEntity entity = _dbSet.Find(id);
@@ -38,25 +114,19 @@
             _dbSet.Remove(entity);
         }
 
-        public IQueryable<TEntity> GetAll()
+        public void Delete(ICollection<TEntity> entities)
         {
-            return _dbSet.AsQueryable();
-        }
+            foreach (var entity in entities.Where(entity => _context.Entry(entity).State == EntityState.Detached))
+            {
+                _dbSet.Attach(entity);
+            }
 
-        public TEntity GetById(object id)
-        {
-            return _dbSet.Find(id);
+            _dbSet.RemoveRange(entities);
         }
 
         public async Task<TEntity> GetByIdAsync(object id)
         {
             return await _dbSet.FindAsync(id);
-        }
-
-        public void Update(TEntity entity)
-        {
-            _dbSet.Attach(entity);
-            _context.Entry(entity).State = EntityState.Modified;
         }
     }
 }
--- App/CompanyManager.Data/Repositories/GenericRepository/IGenericRepository.cs
+++ App/CompanyManager.Data/Repositories/GenericRepository/IGenericRepository.cs
@@ -1,6 +1,9 @@
 ﻿namespace CompanyManager.Data.Repositories.GenericRepository
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
 
     public interface IGenericRepository<TEntity>
@@ -8,16 +11,34 @@
     {
         void Add(TEntity entity);
 
+        void AddRange(IEnumerable<TEntity> entities);
+
         TEntity GetById(object id);
 
-        Task<TEntity> GetByIdAsync(object id);
+        TEntity GetSingle(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] navigationProperties);
+
+        Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] navigationProperties);
+
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] navigationProperties);
+
+        IQueryable<TSubEntity> Get<TSubEntity>(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] navigationProperties) where TSubEntity : TEntity;
 
         IQueryable<TEntity> GetAll();
 
+        void Load<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty>> navigationProperty) where TProperty : class;
+
+        void Load<TCollection>(TEntity entity, Expression<Func<TEntity, ICollection<TCollection>>> navigationProperty) where TCollection : class;
+
         void Update(TEntity entity);
+
+        void PartialUpdate(TEntity entity, object updatedEntity);
 
         void Delete(object id);
 
         void Delete(TEntity entity);
+
+        void Delete(ICollection<TEntity> entities);
+
+        Task<TEntity> GetByIdAsync(object id);
     }
 }
