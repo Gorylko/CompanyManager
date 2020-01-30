039cadc73f88af69176d2befc44ce0313da6a0c2
Yahor Zabotsin <yahor_zabotsin@epam.com>
2020-01-30 19:15:39 +0300

    first commit

--- App/CompanyManager.Web/ExceptionHandling/ExceptionMiddlewareExtensions.cs
+++ App/CompanyManager.Web/ExceptionHandling/ExceptionMiddlewareExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CompanyManager.Web.Middlewares
+﻿namespace CompanyManager.Web.Middlewares
 {
     public class ExceptionMiddlewareExtensions
     {
